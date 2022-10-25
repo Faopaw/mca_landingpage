836d21bc5193bb124cdce6eceea2c2be99df0011
Faopaw <66549556+Faopaw@users.noreply.github.com>
2022-10-25 19:38:41 +0100

    init comitt

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <h1>Breaking into tech. together</h1>
+        <h2>Let us help you attain your dreams of becoming a programmer</h2>
+        <h3>
+          If you're looking for an opportunity to kickstart a career as a
+          Software Developer, we are here to help! Get exclusive, early access
+          to the Mindera Code Academy as soon as we launch it: Fill in the form
+          and get ready to change your life!
+        </h3>
+        <button>Get in Touch!</button>
+        <button>Have a Question?</button>
+      </main>
+    </div>
+  );
+}
+
+export default App;
